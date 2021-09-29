983e85a564a293a5ac63e929d4f717dbec5ec47c
libertus-codelin <91585349+libertus-codelin@users.noreply.github.com>
2021-09-29 13:48:43 +0700

    Menambahkan file tentang manipulasi string pada php

---
+++ manipulasistring.php
@@ -0,0 +1,10 @@
+<?php 
+
+   // cara memanipulasi string pada php
+   $namaDepan = "libertus";
+   $namaBelakang = "sabebeget";
+
+   $namaLengkap = $namaDepan . " " . $namaBelakang;
+   echo $namaLengkap;
+
+?>
